c7fb3623117b0423f61ecee2eb00d006a5018ad1
Brian Dowd <dowdbrian@yahoo.com>
2021-05-06 19:49:32 +0200

    done -css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model.js');
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+      .then(allTheMoviesFromDB => {
+        // console.log('Retrieved books from DB:', allTheMoviesFromDB);
+        res.render('movies.hbs', { movies: allTheMoviesFromDB }); // pass `allTheMoviesFromDB` to the view (as `movies`)
+      })
+      .catch(error => {
+        console.log('Error while getting the movies from the DB: ', error);
+   
+        // Call the error-middleware to display the error page to the user
+        next(error);
+      });
+  });
+
+router.get('/movies/:movieId', (req, res) => {
+  const { movieId } = req.params;
+  
+  Movie.findById(movieId)
+    .then(theMovie => res.render('movie-details', { movie: theMovie }))
+    .catch(error => {
+      console.log('Error while retrieving movie details: ', error);
+  
+      // Call the error-middleware to display the error page to the user
+      next(error);
+    });
+});
 
 module.exports = router;
